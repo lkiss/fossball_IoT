9458b0a626ce1a40ca6e4e87fd7776c21733cfa4
László Kissasdasd <kisslac1988@hotmail.coasdasm>
2019-09-22 20:21:57 +0200

    Refactor to work with cloud functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
-#include <Arduino.h>
-#include "./services/services.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#define WIFI_SSID "PNSC_2_4"
+#define WIFI_PASSWORD "5113111_Cons!"
+
+HTTPClient client;
 
 uint red1Pin = 4;
 uint red2Pin = 5;
@@ -8,47 +12,91 @@ uint blue2Pin = 12;
 uint resetPin = 13;
 uint buttonDelayInMilliseconds = 200;
 
+String blueStriker = "BLUE_STRIKER";
+String blueDefender = "BLUE_DEFENDER";
+String redStriker = "RED_STRIKER";
+String redDefender = "RED_DEFENDER";
+String reset = "RESET";
+
 void setup()
 {
     Serial.begin(9600);
+
     pinMode(red1Pin, INPUT_PULLUP);
     pinMode(red2Pin, INPUT_PULLUP);
     pinMode(blue1Pin, INPUT_PULLUP);
     pinMode(blue2Pin, INPUT_PULLUP);
     pinMode(resetPin, INPUT_PULLUP);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("connected: ");
+    Serial.println(WiFi.localIP());
+}
+
+void sendMatchHistory(String buttonId)
+{
+    client.begin("http://us-central1-hubsson-foosball-eur3.cloudfunctions.net/matchHistory");
+    client.addHeader("Content-Type", "application/json");
+
+    String payload = "{\"buttonId\" :";
+    payload += "\"" + buttonId + "\"}";
+
+    Serial.println("Sending payload");
+    Serial.println(payload);
+
+    int errorCode = client.PUT(payload);
+
+    if (errorCode != HTTP_CODE_OK)
+    {
+        Serial.println(client.errorToString(errorCode));
+    }
+
+    client.end();
 }
 
 void loop()
 {
-    int red1 = digitalRead(red1Pin);
-    int red2 = digitalRead(red2Pin);
-    int blue1 = digitalRead(blue1Pin);
-    int blue2 = digitalRead(blue2Pin);
-    int reset = digitalRead(resetPin);
+    int redStrikerRead = digitalRead(red1Pin);
+    int redDefenderRead = digitalRead(red2Pin);
+    int blueStrikerRead = digitalRead(blue1Pin);
+    int blueDefenderRead = digitalRead(blue2Pin);
+    int resetRead = digitalRead(resetPin);
 
-    if (red1 == LOW)
+    if (redStrikerRead == LOW)
     {
         Serial.println("Red 1 button is pressed");
+        sendMatchHistory(redStriker);
         delay(buttonDelayInMilliseconds);
     }
-    if (red2 == LOW)
+    if (redDefenderRead == LOW)
     {
         Serial.println("Red 2 button is pressed");
+        sendMatchHistory(redDefender);
         delay(buttonDelayInMilliseconds);
     }
-    if (blue1 == LOW)
+    if (blueStrikerRead == LOW)
     {
         Serial.println("Blue 1 button is pressed");
+        sendMatchHistory(blueStriker);
         delay(buttonDelayInMilliseconds);
     }
-    if (blue2 == LOW)
+    if (blueDefenderRead == LOW)
     {
         Serial.println("Blue 2 button is pressed");
+        sendMatchHistory(blueDefender);
         delay(buttonDelayInMilliseconds);
     }
-    if (reset == LOW)
+    if (resetRead == LOW)
     {
         Serial.println("Reset button is pressed");
+        sendMatchHistory(reset);
         delay(buttonDelayInMilliseconds);
     }
 }
--- src/services/services.h
+++
@@ -1,5 +0,0 @@
-#include "./wifi/wifiService.h"
-#include "./data/dataService.h"
-#include "./config/configService.h"
-#include "./json/jsonService.h"
-#include "./server/serverService.h"
